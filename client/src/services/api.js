8f59b4f5cecccb6fd9f1eb36d5532a4b9a42f3d8
Shaik Zulfa Sumayya <sksumayya280@gmail.com>
2025-10-22 12:21:53 +0530

    Use live backend API URL for deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: `${process.env.REACT_APP_API_URL}/api`,
 });
+
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
   if (token) {
